1c633d69d96733bf00447bd03b58548f3b6a6db6
MTonyM <maoymt@gmail.com>
2019-08-09 11:53:18 +0800

    SYNC> Add vis... TODO: - Test phase. - Fix bugs...Training. - Design motion transfer model. - Test & visualization - Metrics (AED, FID) - Fix block expansion issue.

--- vanilla.py
+++ vanilla.py
@@ -19,6 +19,7 @@ from tqdm import tqdm
 from dataloader import DynTexNNFTrainDataset
 from nnf import NNFPredictor, Synthesiser
 from options import TrainOptions
+from vis import Table
 
 
 def train():
@@ -31,41 +32,58 @@ def train():
     syner = Synthesiser().cuda()
     nnfer = NNFPredictor(out_channel=nnf_conf).cuda()
     optimizer_nnfer = Adam(nnfer.parameters(), lr=train_params['lr'])
+    table = Table()
     for epoch in range(opt.epoch):
         pbar = tqdm(total=len(dataloader), desc='epoch#{}'.format(epoch))
         pbar.set_postfix({'loss': 'N/A'})
         loss_tot = 0.0
-        # gamma = epoch / opt.epoch
+        gamma = epoch / opt.epoch
 
         for i, (source_t, target_t, source_t1, target_t1) in enumerate(dataloader):
             source_t = Variable(source_t, requires_grad=True).cuda()
             target_t = Variable(target_t, requires_grad=True).cuda()
-            # source_t1 = Variable(source_t1, requires_grad=True).cuda()
-            # target_t1 = Variable(target_t1, requires_grad=True).cuda()
+            source_t1 = Variable(source_t1, requires_grad=True).cuda()
+            target_t1 = Variable(target_t1, requires_grad=True).cuda()
             nnf = nnfer(source_t, target_t)
             if nnf_conf == 3:
                 nnf = nnf[:, :2, :, :] * nnf[:, 2:, :, :]  # mask via the confidence
             # --- synthesis ---
             target_predict = syner(source_t, nnf)
-            # target_t1_predict = syner(source_t1, nnf)
-
-            loss = tnf.mse_loss(target_predict, target_t)
-            # + gamma * tnf.mse_loss(target_t1_predict, target_t1)
+            target_t1_predict = syner(source_t1, nnf)
+            loss_t = tnf.mse_loss(target_predict, target_t)
+            loss_t1 = tnf.mse_loss(target_t1_predict, target_t1)
+            loss = loss_t + gamma * loss_t1
 
             optimizer_nnfer.zero_grad()
             loss.backward()
             optimizer_nnfer.step()
-            loss_tot += float(loss)
+            loss_tot += float(loss_t1)
 
             # ---   vis    ---
-            name = os.path.join(data_root, '../result', '{}.png'.format(str(i)))
-            cv2.imwrite(name.replace('.png', '{}_t0P.png'.format(epoch)),
-                        (target_predict.detach().cpu().numpy()[0].transpose(1, 2, 0) * 255).astype('int'))
-            cv2.imwrite(name.replace('.png', '{}_t0T.png'.format(epoch)),
+            name = os.path.join(data_root, '../result/', str(epoch), '{}.png'.format(str(i)))
+            index = str(epoch) + '({})'.format(i)
+            if not os.path.exists('/'.join(name.split('/')[:-1])):
+                os.makedirs('/'.join(name.split('/')[:-1]))
+            cv2.imwrite(name.replace('.png', '_t.png'),
                         (target_t.detach().cpu().numpy()[0].transpose(1, 2, 0) * 255).astype('int'))
+
+            cv2.imwrite(name.replace('.png', '_p.png'),
+                        (target_predict.detach().cpu().numpy()[0].transpose(1, 2, 0) * 255).astype('int'))
+
+            cv2.imwrite(name.replace('.png', '_t1.png'),
+                        (target_t1.detach().cpu().numpy()[0].transpose(1, 2, 0) * 255).astype('int'))
+
+            cv2.imwrite(name.replace('.png', '_p1.png'),
+                        (target_t1_predict.detach().cpu().numpy()[0].transpose(1, 2, 0) * 255).astype('int'))
+
+            # vis in table
+            table.add(index, name.replace('.png', '_t.png'))
+            table.add(index, name.replace('.png', '_p.png'))
+            table.add(index, name.replace('.png', '_t1.png'))
+            table.add(index, name.replace('.png', '_p1.png'))
             pbar.set_postfix({'loss': str(loss_tot / (i + 1))})
             pbar.update(1)
-
+        table.build_html('data/')
         pbar.close()
 
 
---
+++ vis.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+@Project:   DynTexTrans
+@File   :   vis
+@Author :   TonyMao@AILab
+@Date   :   2019-08-09
+@Desc   :   None
+"""
+
+import os
+
+"""
+	<tr>
+		<td>0</td>
+		<td><img width="128" src="result/gt.png"></td>
+		<td><img width="128" src="result/gt.png"></td>
+		<td><img width="128" src="result/gt.png"></td>
+	</tr>
+"""
+
+
+class Table(object):
+    def __init__(self):
+        self.img_list = {}
+        self.header = """
+<html class="gr__10_224_7_179"><head>
+</head>
+
+<body data-gr-c-s-loaded="true">
+<style>* {font-size: 24px;}</style><table>
+<tbody>
+	<tr>
+		<td>index</td>
+		<td>Tt</td>
+		<td>PrTt</td>
+		<td>Tt+1</td>
+		<td>PrTt+1</td>
+	</tr>
+"""
+        self.tail = """
+</tbody></table>
+</body></html>
+        """
+
+        self.format = """
+    <tr>
+		<td>{}</td>
+		<td><img src="{}"></td>
+		<td><img src="{}"></td>
+		<td><img src="{}"></td>
+		<td><img src="{}"></td>
+	</tr>"""
+
+    def add(self, key: str, url: str):
+        if not key in self.img_list.keys():
+            self.img_list[key] = []
+        else:
+            self.img_list[key].append(url)
+
+    def build_html(self, path):
+        html = self.header
+        for key in sorted(self.img_list.keys()):
+            html += self.format.format(key, *self.img_list[key])
+        html += self.tail
+        with open(os.path.join(path, 'vis.html'), 'w') as f:
+            f.write(html)
+        f.close()
